2a1dfeb20c7cf38c0f79dc0dcdad19c3a05d7fc5
Siddhi9811 <siddhimishra9811@gmail.com>
2026-01-22 13:09:46 +0530

    Updated Question1

--- Question1.java
+++ Question1.java
@@ -1,6 +1,6 @@
 public class Question1{
     public static void main(String[] args) {
-        for(int i =1; i<=10; i++){
+        for(int i =1; i < =10; i++){
             System.out.println("Printing Numbers from 1 to 10 : "  + i);
         }
     }
