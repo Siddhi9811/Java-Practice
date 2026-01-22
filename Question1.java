79e2405f98ae80db352d7d1750c35cc8e244a6ed
Siddhi9811 <siddhimishra9811@gmail.com>
2026-01-22 17:14:53 +0530

    Delete Question1.java

--- Question1.java
+++
@@ -1,7 +0,0 @@
-public class Question1{
-    public static void main(String[] args) {
-        for(int i =1; i < =10; i++){
-            System.out.println("Printing Numbers from 1 to 10 : "  + i);
-        }
-    }
-}
